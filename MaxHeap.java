387d8bcaa4a7fee78ffd872403e628e12fc8fd29
Ishan Sharma <ishan.sharma091@gmail.com>
2017-03-10 09:36:47 -0500

    Create MaxHeap.java

---
+++ MaxHeap.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+
+public class MaxHeap {
+	private int size = 0;
+	private int capacity = 10;
+	private int[] elements = new int[capacity];
+	
+	private int getLeftChildIndex (int index) {return (2 * index + 1);}
+	private int getRightChildIndex (int index) {return (2 * index + 2);}
+	private int getParentIndex (int index) {return (index - 1) / 2;}
+	
+	private boolean hasLeftChild(int index) {return getLeftChildIndex(index) < size;}
+	private boolean hasRightChild(int index) {return getRightChildIndex(index) < size;}
+	private boolean hasParent(int index) {return getParentIndex(index) >= 0;}
+	
+	private int getLeftChild(int index) {return elements[getLeftChildIndex(index)];}
+	private int getRightChild(int index) {return elements[getRightChildIndex(index)];}
+	private int getParent(int index) {return elements[getParentIndex(index)];}
+	
+	private void checkCapacity() {
+		if (size == capacity) {
+			capacity *= 2;
+			elements = Arrays.copyOf(elements, capacity);
+		}
+	}
+	
+	private void swap(int index1, int index2) {
+		int temp = elements[index1];
+		elements[index1] = elements[index2];
+		elements[index2] = temp;
+	}
+	
+	public void insert(int value) {
+		checkCapacity();
+		elements[size] = value;
+		size++;
+		heapifyUp();
+	}
+	
+	private void heapifyUp() {
+		int index = size - 1;
+		while(hasParent(index) && getParent(index) < elements[index]) {
+			swap(getParentIndex(index), index);
+			index = getParentIndex(index);
+		}	
+	}
+	
+	public int poll() {
+		if (size == 0) throw new IllegalStateException();
+		int holdValue = elements[0];
+		elements[0] = elements[size - 1];
+		size--;
+		heapifyDown();
+		return holdValue;
+	}
+	
+	private void heapifyDown() {
+		int index = 0;
+		while(hasLeftChild(index)) {
+			int largerIndex = getLeftChildIndex(index);
+			if (hasRightChild(index) && getLeftChild(index) < getRightChild(index)) {
+				largerIndex = getRightChildIndex(index);
+			}
+			if (elements[index] > elements[largerIndex]) {
+				break;
+			}
+			swap(index, largerIndex);
+			index = largerIndex;
+		}
+	}
+	
+	public void printHeap() {
+		for (int i = 0; i < size; i++) {
+			System.out.println(elements[i]);
+		}
+	}
+	
+	public void heapSort() {
+		int maxIndex = size;
+		while(size != 0) {
+			swap(0, size-1);
+			size--;
+			heapifyDown();
+		}
+		size = maxIndex;
+	}
+	
+	public static void main(String args[]) {
+		MaxHeap h1 = new MaxHeap();
+		h1.insert(100);
+		h1.insert(-1);
+		h1.insert(26);
+		h1.insert(-80);
+		h1.insert(10);
+		h1.insert(101);
+		h1.heapSort();
+		h1.printHeap();
+
+	}
+}
